30085d352940fbdefacaaf71a8705b7d09b53c39
Francisco de Assis Boldt <fboldt@gmail.com>
2022-07-26 09:51:10 -0300

    include http-status-codes library

--- auth/controller.js
+++ auth/controller.js
@@ -1,3 +1,4 @@
+const { StatusCodes } = require('http-status-codes')
 const jwt = require('jsonwebtoken')
 
 const login = async (req, res) => {
@@ -5,13 +6,13 @@ const login = async (req, res) => {
     if (username === 'alice' && password === '123') {
         const token = jwt.sign({ username }, process.env.JWT_SECRET,
             { expiresIn: process.env.JWT_LIFETIME })
-        return res.status(200).json({ username, token })
+        return res.status(StatusCodes.OK).json({ username, token })
     }
-    return res.status(401).json({})
+    return res.status(StatusCodes.FORBIDDEN).json({})
 }
 
 const logout = async (req, res) => { 
-    return res.status(200).json({ username: null, token: null })
+    return res.status(StatusCodes.OK).json({ username: null, token: null })
 }
 
 module.exports = {
