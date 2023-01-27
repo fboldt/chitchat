95371374832bd739d5685c46e95818463a1a820b
Francisco de Assis Boldt <fboldt@gmail.com>
2023-01-27 11:22:04 -0300

    create and use async version of getQuote.

--- index.mjs
+++ index.mjs
@@ -14,20 +14,23 @@ app.use(express.static(__dirname + '/public'))
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    res.render('home', { quote: getQuote() })
+    getQuote((err, data) => {
+        if (err) {
+            res.status(500).render('500')
+        }
+        res.render('home', { quote: data })
+    })
 })
 
 app.get('/sobre', (req, res) => res.render('sobre'))
 
 app.use((req, res) => {
-    res.status(404)
-    res.render('404')
+    res.status(404).render('404')
 })
 
 app.use((err, req, res, next) => {
     console.error(err.message)
-    res.status(500)
-    res.render('500')
+    res.status(500).render('500')
 })
 
 app.listen(port, () => console.log(`sevidor iniciado na porta ${port},` +
