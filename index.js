a0e23c8b8a6a5cf9f7d8a521a08d6f75cd746943
Francisco de Assis Boldt <fboldt@gmail.com>
2023-01-28 07:12:02 -0300

    adiciona testes de integração

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { engine } from 'express-handlebars'
 import path from 'path';
+import * as url from 'node:url';
 import handlers from './lib/handlers.js';
 
 const app = express()
@@ -21,5 +22,12 @@ app.use(handlers.notFound)
 
 app.use(handlers.serverError)
 
-app.listen(port, () => console.log(`sevidor iniciado na porta ${port},` +
-    ' pressione Ctrl+c para terminar...'))
+if (import.meta.url.startsWith('file:')) { 
+    const modulePath = url.fileURLToPath(import.meta.url);
+    if (process.argv[1] === modulePath) { 
+        app.listen(port, () => console.log(`sevidor iniciado na porta ${port},` +
+            ' pressione Ctrl+c para terminar...'))
+    }
+}
+
+export default app
---
+++ integration-tests/basic-navigation.test.js
@@ -0,0 +1,29 @@
+import { expect, jest, test, beforeEach, afterEach } from '@jest/globals';
+
+import portfinder from 'portfinder'
+import puppeteer from 'puppeteer'
+import app from '../index'
+
+let server = null
+let port = null
+
+beforeEach(async () => {
+    port = await portfinder.getPortPromise()
+    server = app.listen(port)
+})
+
+afterEach(() => {
+    server.close()
+})
+
+test('link da página inicial para a página sobre', async () => {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto(`http://localhost:${port}`)
+    await Promise.all([
+        page.waitForNavigation(),
+        page.click('[data-test-id="sobre"'),
+    ])
+    expect(page.url()).toBe(`http://localhost:${port}/sobre`)
+    await browser.close()
+})
